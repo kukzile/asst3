669469e0128a9f1ad8ec4743edf03616b798cb4d
Kukzile <kriti.kukreja@gmail.com>
2023-10-12 02:20:46 -0400

    parallelfor and sequence

--- src/parallel-for-simulator.cpp
+++ src/parallel-for-simulator.cpp
@@ -2,6 +2,7 @@
 #include "world.h"
 #include <algorithm>
 #include <iostream>
+#include <omp.h>
 
 // TASK 2.2
 
@@ -10,14 +11,42 @@
 // the same. You may modify any code outside this class unless otherwise
 // specified.
 
-const int QuadTreeLeafSize = 8;
+const int QuadTreeLeafSize = 128;
+
 class ParallelForNBodySimulator : public INBodySimulator {
 public:
   // TODO: implement a function that builds and returns a quadtree containing
   // particles. You do not have to preserve this function type.
   std::unique_ptr<QuadTreeNode> buildQuadTree(std::vector<Particle> &particles,
                                               Vec2 bmin, Vec2 bmax) {
-    return nullptr;
+
+    std::unique_ptr<QuadTreeNode> quadTree = std::make_unique<QuadTreeNode>();
+    if (particles.size() <= QuadTreeLeafSize) {  
+        quadTree->isLeaf = true;
+        quadTree->particles = particles;
+    } else {
+        quadTree->isLeaf = false;
+        Vec2 mid = (bmin + bmax) * 0.5f;
+        std::vector<std::vector<Particle>> children(4);
+        for (int i = 0; i < particles.size(); i++){
+          auto p = particles[i];
+          int posx, posy;
+          if (p.position.x < mid.x) 
+            posx = 0;
+          else posx = 1;
+          if (p.position.y < mid.y) 
+            posy = 0;
+          else posy = 1;
+          children[posx + 2 * posy].push_back(p);
+        }
+        Vec2 b_mins[] = {bmin, Vec2(mid.x, bmin.y), Vec2(bmin.x, mid.y), mid};
+        Vec2 b_maxs[] = {mid, Vec2(bmax.x, mid.y), Vec2(mid.x, bmax.y), bmax};
+        #pragma omp parallel for schedule(static) if (particles.size() > 400)
+        for (int i = 0; i < 4; i++){
+            quadTree->children[i] = buildQuadTree(children[i], b_mins[i], b_maxs[i]);
+        }
+    }
+    return quadTree;
   }
 
   // Do not modify this function type.
@@ -56,6 +85,21 @@ public:
                             StepParameters params) override {
     // TODO: implement parallel version of quad-tree accelerated n-body
     // simulation here, using quadTree as acceleration structure
+    #pragma omp parallel for schedule(dynamic, 4)
+    for (int i = 0; i < (int)particles.size(); i++) {
+        auto pi = particles[i];
+        Vec2 force = Vec2(0.0f, 0.0f);
+        std::vector<Particle> nearParticles;
+        accel->getParticles(nearParticles, pi.position, params.cullRadius);
+        // accumulate attractive forces to apply to particle i
+        for (size_t j = 0; j < (int)nearParticles.size(); j++) {
+          if (nearParticles[j].id == pi.id) continue;
+          if ((pi.position - nearParticles[j].position).length() < params.cullRadius)
+            force += computeForce(pi, nearParticles[j], params.cullRadius);
+        }
+        // update particle state using the computed force
+        newParticles[i] = updateParticle(pi, force, params.deltaTime);
+      }
   }
 };
 
--- src/seq-simulator.cpp
+++ src/seq-simulator.cpp
@@ -8,16 +8,39 @@
 // NOTE: You may modify any of the contents of this file, but preserve all
 // function types and names. You may add new functions if you believe they will
 // be helpful.
-
 const int QuadTreeLeafSize = 8;
 class SequentialNBodySimulator : public INBodySimulator {
-public:
   std::unique_ptr<QuadTreeNode> buildQuadTree(std::vector<Particle> &particles,
                                               Vec2 bmin, Vec2 bmax) {
     // TODO: implement a function that builds and returns a quadtree containing
-    // particles.
-    return nullptr;
+    // particles
+    std::unique_ptr<QuadTreeNode> quadTree = std::make_unique<QuadTreeNode>();
+        if (particles.size() <= QuadTreeLeafSize) {  
+        quadTree->isLeaf = true;
+        quadTree->particles = particles;
+    } else {
+        quadTree->isLeaf = false;
+        Vec2 mid = (bmin + bmax) * 0.5f;
+        std::vector<Particle> children[4];
+        for (int i = 0; i < particles.size(); i++){
+          auto p = particles[i];
+          int posx, posy;
+          if (p.position.x < mid.x) 
+            posx = 0;
+          else posx = 1;
+          if (p.position.y < mid.y) 
+            posy = 0;
+          else posy = 1;
+          children[posx + 2 * posy].push_back(p);
+        }
+        quadTree->children[0] = buildQuadTree(children[0], bmin, mid);
+        quadTree->children[1] = buildQuadTree(children[1], Vec2(mid.x, bmin.y), Vec2(bmax.x, mid.y));
+        quadTree->children[2] = buildQuadTree(children[2], Vec2(bmin.x, mid.y), Vec2(mid.x, bmax.y));
+        quadTree->children[3] = buildQuadTree(children[3], mid, bmax);
+    }
+    return quadTree;
   }
+  
   virtual std::unique_ptr<AccelerationStructure>
   buildAccelerationStructure(std::vector<Particle> &particles) {
     // build quad-tree
@@ -36,6 +59,8 @@ public:
 
     quadTree->bmin = bmin;
     quadTree->bmax = bmax;
+    printf("bmin: (%f, %f)\n", bmin.x, bmin.y);
+    printf("bmax: (%f, %f)\n", bmax.x, bmax.y);
 
     // build nodes
     quadTree->root = buildQuadTree(particles, bmin, bmax);
@@ -51,6 +76,20 @@ public:
                             StepParameters params) override {
     // TODO: implement sequential version of quad-tree accelerated n-body
     // simulation here, using quadTree as acceleration structure
+    #pragma omp parallel for
+    for (int i = 0; i < (int)particles.size(); i++) {
+        auto pi = particles[i];
+        Vec2 force = Vec2(0.0f, 0.0f);
+        std::vector<Particle> nearParticles;
+        accel->getParticles(nearParticles, pi.position, params.cullRadius);
+        // accumulate attractive forces to apply to particle i
+        for (size_t j = 0; j < (int)nearParticles.size(); j++) {
+          if ((pi.position - nearParticles[j].position).length() < params.cullRadius)
+            force += computeForce(pi, nearParticles[j], params.cullRadius);
+        }
+        // update particle state using the computed force
+        newParticles[i] = updateParticle(pi, force, params.deltaTime);
+      }
   }
 };
 
